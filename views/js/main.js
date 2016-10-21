284a68ba85515e7745874e30728f41ed14879508
Jennifer Almeida <jenniferlmd793@gmail.com>
2016-10-21 09:50:13 -0400

    added intViewPortWidth

--- views/js/main.js
+++ views/js/main.js
@@ -573,8 +573,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	//moved out of for loop, put into var and replaced querySelector
 	var movingPizzas = document.getElementById("movingPizzas1");
 	
+	//Referenced Karol's suggestion on dynamically calculating background pizzas ("I still have FSL issues!" discussion)
+	var intViewportWidth = window.innerWidth;
+	
 	//decreased pizzas to 32 in for loop (32 and 256 are multiples of 8) 
-  for (var i = 0; i < 32; i++) { 
+  for (var i = 0, elem; i < 32; i++) { 
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
